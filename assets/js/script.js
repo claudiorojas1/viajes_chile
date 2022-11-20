97ffd99b5b8e21696e4ae5a9a5503facac5b0f3c
claudiorojas <claudiorojas1@gmail.com>
2022-11-20 17:12:29 -0300

    correccion del footer

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var tooltipTriggerList =
+[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$('.validarnombre1').click(function(){
+    alert()
+});
+    
+console.log
+
